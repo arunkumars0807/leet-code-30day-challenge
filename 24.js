467a79913bc930c7c90df9ba804d2c1f5871f354
arunkumars0807 <62843045+arunkumars0807@users.noreply.github.com>
2020-04-24 20:43:46 +0530

    Update 24.js

--- 24.js
+++ 24.js
@@ -1,3 +1,61 @@
+// Approach 1: 
+
+// Using JS Map
+
+/**
+ * @param {number} capacity
+ */
+var LRUCache = function(capacity) {
+    this.cache = new Map();
+    this.capacity = capacity;
+};
+
+/** 
+ * @param {number} key
+ * @return {number}
+ */
+LRUCache.prototype.get = function(key) {
+    if (this.cache.has(key)) {
+        var value = this.cache.get(key);
+        this.cache.delete(key);
+        this.cache.set(key, value);
+        return value;
+    }
+    return -1;
+};
+
+/** 
+ * @param {number} key 
+ * @param {number} value
+ * @return {void}
+ */
+LRUCache.prototype.put = function(key, value) {
+    if (this.cache.has(key)) {
+        this.cache.delete(key);
+    }
+    
+    this.cache.set(key, value);
+    
+    if (this.cache.size > this.capacity) {
+        this.cache.delete(this.cache.keys().next().value);
+    }
+};
+
+/** 
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+
+
+
+
+// Approach 2:
+
+// Using Doubly LinkedList and HashMap
+
+
 var List = function(key, value) {
     this.key = key;
     this.value = value;
